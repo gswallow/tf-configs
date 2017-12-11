954039aaf84d69722ffee48b439581c4b0309e39
Greg Swallow <gswallow@ivytech.edu>
2017-12-10 19:50:06 -0500

    Add an internal zone.  Lots may change here.

---
+++ vpc/route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_zone" "main" {
+  name = "${format("%s.%s", terraform.workspace, var.ORG)}"
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+    Environment = "${terraform.workspace}"
+  }
+}
