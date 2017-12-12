9adfd662faa5dd5919c773b545e749608f69525b
Greg Swallow <gswallow@ivytech.edu>
2017-12-12 16:31:01 -0500

    Use comments to work around the aws_route53_zone data source

--- vpc/route53.tf
+++ vpc/route53.tf
@@ -1,5 +1,6 @@
 resource "aws_route53_zone" "main" {
   name = "${format("%s.%s", terraform.workspace, var.ORG)}"
+  comment = "${format("%s.%s", terraform.workspace, var.ORG)}"
   vpc_id = "${aws_vpc.main.id}"
   tags {
     Environment = "${terraform.workspace}"
