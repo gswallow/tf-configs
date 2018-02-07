f28c2425a0e4a3e221490ce6075590bb3d5e3ad6
Greg Swallow <gswallow@ivytech.edu>
2018-02-07 11:14:56 -0500

    Add a private zone module

--- configs/route53/zones.tf
+++ configs/route53/zones.tf
@@ -1,6 +1,13 @@
-module "example" { 
-  source = "../../modules/route53_public_zone"
-  zone_name = "example.ivytech.edu"
-}
+# Examples
 
-# just copy pasta...
+# module "external" { 
+#   source = "../../modules/route53_public_zone"
+#   zone_name = "external.ivytech.edu"
+#   log_retention = 7
+# }
+
+# module "internal" {
+#   source = "../../modules/route53_private_zone"
+#   zone_name = "internal.ivytech.edu"
+#   vpc_id = "${data.aws_vpc.selected.id}"
+# }
