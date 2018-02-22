7f8f6f953f1a02411ab8055d3d1e607877bdc681
Greg Swallow <gswallow@ivytech.edu>
2018-02-22 15:29:54 -0500

    Limit to 4 availability zones.

--- configs/vpc/vpc.tf
+++ configs/vpc/vpc.tf
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "main" {
 
 # Public subnets.  Easy Peasy.
 resource "aws_subnet" "public" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   vpc_id = "${aws_vpc.main.id}"
   availability_zone = "${element(data.aws_availability_zones.available.names, count.index)}"
   cidr_block = "172.${var.CIDR_BLOCK}.${var.CIDR_PORTION + count.index}.0/24"
@@ -41,18 +41,19 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
 # NATted subnets require NAT gateways and route tables.
 resource "aws_eip" "nat" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
+  vpc = true
 }
 
 resource "aws_nat_gateway" "gw" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   tags {
@@ -61,7 +62,7 @@ resource "aws_nat_gateway" "gw" {
 }
 
 resource "aws_subnet" "nat" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   vpc_id = "${aws_vpc.main.id}"
   availability_zone = "${element(data.aws_availability_zones.available.names, count.index)}"
   cidr_block = "172.${var.CIDR_BLOCK}.${var.CIDR_PORTION + 8 + count.index * 4}.0/22"
@@ -73,7 +74,7 @@ resource "aws_subnet" "nat" {
 }
 
 resource "aws_route_table" "nat" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   vpc_id = "${aws_vpc.main.id}"
   route {
     cidr_block = "0.0.0.0/0"
@@ -86,14 +87,14 @@ resource "aws_route_table" "nat" {
 }
 
 resource "aws_route_table_association" "nat" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   subnet_id = "${element(aws_subnet.nat.*.id, count.index)}"
   route_table_id = "${element(aws_route_table.nat.*.id, count.index)}"
 }
 
 # Private (no internet) subnets
 resource "aws_subnet" "private" {
-  count = "${length(data.aws_availability_zones.available.names)}"
+  count = "${min(length(data.aws_availability_zones.available.names), 4)}"
   vpc_id = "${aws_vpc.main.id}"
   availability_zone = "${element(data.aws_availability_zones.available.names, count.index)}"
   cidr_block = "172.${var.CIDR_BLOCK}.${var.CIDR_PORTION + 60 - count.index * 4}.0/22"
